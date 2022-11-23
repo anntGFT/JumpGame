6a7898a0320d3984fb921d01881dfd70404b639d
annt <annt@gft.com>
2022-11-23 16:10:14 +0000

    Done with simple for loop

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,35 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public boolean canJump(int[] nums) {
+
+        int jumps = 0;
+
+        if (nums.length == 1){
+
+            return true;
+        }
+
+        for (int i = 0; i < nums.length; i++){
+
+            if (jumps <= nums[i]){
+
+                jumps = nums[i];
+            }else{
+
+                jumps--;
+            }
+
+            if (jumps == 0 && i != nums.length - 1){
+
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,60 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    int[] nums;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void ArrayWith1NumbersAlwaysCanJump(){
+
+        nums = new int[] {0};
+
+        assertTrue(solution.canJump(nums));
+    }
+
+    @Test
+    public void ArrayWith2NumbersAndCanJump(){
+
+        nums = new int[] {1,0};
+
+        assertTrue(solution.canJump(nums));
+    }
+
+    @Test
+    public void ArrayWith2NumbersAndCantJump(){
+
+        nums = new int[] {0,0};
+
+        assertFalse(solution.canJump(nums));
+    }
+
+    @Test
+    public void ArrayWith5NumbersAndCanJump(){
+
+        nums = new int[] {2,2,0,1,4};
+
+        assertTrue(solution.canJump(nums));
+    }
+
+    @Test
+    public void ArrayWith5NumbersAndCantJump(){
+
+        nums = new int[] {3,2,1,0,4};
+
+        assertFalse(solution.canJump(nums));
+    }
 }
